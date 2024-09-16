6975e34619bdb645f0e559cd72e8bea5f0bb2002
octoross <codingoceane@gmail.com>
2024-09-16 20:14:48 +0200

    fix(Display-of-user-prompt): Ajout du cas de reussite de getcwd

--- src/display/input.c
+++ src/display/input.c
@@ -6,7 +6,7 @@
 /*   By: octoross <octoross@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 15:31:32 by octoross          #+#    #+#             */
-/*   Updated: 2024/09/16 19:19:37 by octoross         ###   ########.fr       */
+/*   Updated: 2024/09/16 20:12:13 by octoross         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,29 +17,42 @@ char	*ft_get_prompt(void)
 	char	wd[PATH_SIZE];
 	char	*prompt;
 	int		len;
+	int		err;
 
-	len = ft_strlen("minishell: ") + ft_strlen(CYAN) + ft_strlen(END) + 2;
+	len = ft_strlen("🐳 minishell:") + ft_strlen(CYAN) + ft_strlen(END) + 2;
+	err = 0;
 	if (!getcwd(wd, PATH_SIZE))
 	{
 		len += ft_strlen(ERR_GETCWD);
-		prompt = (char *)malloc(sizeof(char) * (len + 1));
-		if (!prompt)
-			return (ft_fail(ERR_MALLOC), NULL);
-		prompt[len] = '\0';
-		ft_strcpy(prompt, CYAN);
-		len = ft_strlen(CYAN);
-		ft_strcpy(&prompt[len], "minishell: ");
-		len += ft_strlen("minishell: ");
+		err = 1;
+	}
+	else
+		len += ft_strlen(wd) + ft_strlen(BLUE);
+	prompt = (char *)malloc(sizeof(char) * (len + 1));
+	if (!prompt)
+		return (ft_fail(ERR_MALLOC), NULL);
+	prompt[len] = '\0';
+	ft_strcpy(prompt, CYAN);
+	len = ft_strlen(CYAN);
+	ft_strcpy(&prompt[len], "🐳 minishell:");
+	len += ft_strlen("🐳 minishell:");
+	if (!err)
+	{
+		ft_strcpy(&prompt[len], BLUE);
+		len += ft_strlen(BLUE);
+		ft_strcpy(&prompt[len], wd);
+		len += ft_strlen(wd);
+		ft_strcpy(&prompt[len], END);
+		len += ft_strlen(END);
+	}
+	else
+	{
 		ft_strcpy(&prompt[len], END);
 		len += ft_strlen(END);
 		ft_strcpy(&prompt[len], ERR_GETCWD);
 		len += ft_strlen(ERR_GETCWD);
-		ft_strcpy(&prompt[len], "> ");
 	}
-	else
-	{
-		return (NULL);
-	}
+	ft_strcpy(&prompt[len], "> ");
 	return (prompt);
 }
 
